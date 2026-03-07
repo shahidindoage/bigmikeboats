dfdf8ac7ab0de0c67284c3a3d019f8afc7764fa6
shahidindoage <shahid.indoage@gmail.com>
2026-03-07 12:43:47 +0530

    Comment out unused imports in server.js
    Commented out unused imports for path and static middleware.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
 const dotenv = require("dotenv");
-
+// const path = require('path'); 
 
 dotenv.config();
 
@@ -11,6 +11,7 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(cors());
 app.use(express.json());
+// app.use(express.static(path.join(__dirname, 'public')));
 
 // Main page - loads with all boats by default
 app.get("/", async (req, res) => {
